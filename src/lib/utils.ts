8908efe4f16893e8b91c67d149010ebcc9de80d3
Josh <joshvantsant@gmail.com>
2026-08-19 15:37:50 +0200

    Add isVideoSrc function to validate video sources

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isVideoSrc(src: string): boolean {
+  return /\.(mp4|webm|mov)$/i.test(src);
+}
